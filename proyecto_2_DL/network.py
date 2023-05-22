80e4c92a4fd7546b14f72431a5735c51c7000bb2
Francisco Durazo <fcodl2001@gmail.com>
2023-05-21 18:53:35 -0700

    Finished Network, not tested yet

--- proyecto_2_DL/network.py
+++ proyecto_2_DL/network.py
@@ -53,7 +53,7 @@ class Network(nn.Module):
         '''
         models_path = file_path / 'models' / model_name
         # TODO: Guarda los pesos de tu red neuronal en el path especificado
-        torch.save( ... )
+        torch.save(self.state_dict(), models_path)
 
     def load_model(self, model_name: str):
         '''
@@ -62,3 +62,5 @@ class Network(nn.Module):
             - path (str): path relativo donde se guardó el modelo
         '''
         # TODO: Carga los pesos de tu red neuronal
+        models_path = file_path / 'models' / model_name
+        self.load_state_dict(torch.load(models_path))
